ac80a57b0cc3103417ee5ce0a2c371413d8c4285
Aswin Raghuprasad <aswinrprasad@gmail.com>
2019-11-02 00:14:34 +0530

    Basics

--- learn.hs
+++ learn.hs
@@ -51,3 +51,31 @@ quicksort (x:xs) =
 
 -- char positions 
 pos string key = [i | i <- [0..((length string)-1)] , (string !! i) == key]  
+
+-- Higher Order Functions 
+applyTwice f n = f $ f n
+
+-- Flip function 
+flip' f x y = f y x
+
+-- map function
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+--Finding largest number under 10000 divisible by 3829
+largdiv = head $ filter (\x -> x `mod` 3829 == 0) [10000,9999..]
+
+-- Collatz Sequence
+chain 1 = [1]
+chain n 
+    | even n = n: chain  (n `div` 2)
+    | odd n = n: chain (n*3 + 1)
+
+--foldl , foldr , scanl , scanr , foldl1 ,foldr1 ,scanl1 , scanr1
+
+sum' xs = foldl (\acc x-> acc + x ) 0 xs 
+--sumx = foldl (+) 0
+
+map1 f xs = foldl (\acc x -> acc ++ [f x] ) [] xs 
+
+reverse2 xs= foldr (\x acc -> acc++[x] ) [] xs

838e4bf2df38d61e26a1de9bc3f5a8a4c2090408
Aswin Raghuprasad <aswinrprasad@gmail.com>
2019-11-01 11:42:54 +0530

    Data.Map Module

--- ES_prep3.hs
+++ ES_prep3.hs
@@ -20,3 +20,10 @@ findKey' key = foldr (\x acc -> if fst x == key then snd x else acc) "Not Found"
 
 -- Create a map from an empty fromList
 ins xs= foldr (\(k,v) acc -> Map.insert k v acc) Map.empty xs
+
+-- Using fromListWith : Takes a List and a function as parameters, if there are duplicate keys , the (k,v) pair is supplied to the function and function decides how it behaves.
+phoneBookToMap xs = Map.fromListWith (\number1 number2 -> number1 ++ " , " ++number2 ) xs
+
+-- insertWith : Accepts a Map Association List and a function
+insertToFromlist k v xs = Map.insertWith (\number1 number2 -> number1 ++ " , " ++number2 ) k v xs
+
